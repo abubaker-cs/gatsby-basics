0513a337d9551f73c9edf98e48645f19af698e81
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-04 14:57:09 +0500

    Understanding developing navigation flow in gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const about = () => {
+  return (
+    <div>
+      <h2>Hello from about page</h2>
+    </div>
+  )
+}
+
+export default about
---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const history = () => {
+  return <div>Company - History</div>
+}
+
+export default history
