66205a2762f4ee7776c3d5b842652a35532cff51
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-05 00:30:48 +0500

    Completed: Understanding Basics

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout"
-import * as aboutStyles from "../examples/about.module.css"
+// import * as aboutStyles from "../examples/about.module.css"
+import styled from "styled-components"
 
 const about = () => {
   return (
     <Layout>
-      <div className={aboutStyles.page}>
+      <Wrapper>
         <h1>About</h1>
-        <p>
+        <p className="text">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, nihil
           aliquid. Aspernatur beatae debitis iure voluptas perferendis
           laudantium corporis consectetur!
@@ -15,9 +16,27 @@ const about = () => {
 
         <h1>Section</h1>
         <p>Lorem ipsum dolor sit amet.</p>
-      </div>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: green;
+  background-color: #f0f0f0;
+  padding: 1rem;
+  border: 1px solid black;
+
+  h1 {
+    color: blueviolet;
+    padding: 1rem;
+    border: 1px solid black;
+    font-size: 3rem;
+  }
+
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { page, text } from "../examples/home.module.css"
+import { ExampleButton } from "../examples/button"
 
 export default function Home() {
   return (
     <Layout>
       <div className={page}>
+        <ExampleButton>Click Me</ExampleButton>
         <h1>Home page</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit mollitia
